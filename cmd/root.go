17f7a17a1a076909acba297c98d90c3b4efee6e7
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-07-16 21:14:00 -0400

    feat: WIP - add root file for base

---
+++ cmd/root.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"dial/internal/store"
+	"dial/internal/tracker"
+)
+
+var (
+	db *sql.Db
+	trk *tracker.Tracker
+)
+
+var rootCmd = &cobra.Command{
+	Use: "dial",
+	Short: "Dial - quick time tracking from the CLI and GUI",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		path, err := store.DefaultPath()
+		if err != nil {
+			return err
+		}
+		db, err = store.Open(path)
+		if err != nil {
+			return err
+		}
+		trk = tracker.New(db)
+		return nil
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		if db != nil {
+			db.Close()
+		}
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.FPrintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
